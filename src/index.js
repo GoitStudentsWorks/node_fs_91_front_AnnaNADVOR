e3fac11d079ba18d99c2ab8839cc9aac1837041c
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2024-04-01 02:02:21 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/taskPro_project-group-6">
+      <BrowserRouter basename="/node_fs_91_front_AnnaNADVOR">
         <App />
       </BrowserRouter>
     </Provider>
